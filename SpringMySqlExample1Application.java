cc17d9d43644322d029ca426df3952cc3e8497cf
RajanigandhaWorkspace <145898646+RajanigandhaWorkspace@users.noreply.github.com>
2023-09-24 14:49:10 +0530

    Add files via upload
    Changes about database creation.

---
+++ SpringMySqlExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMySqlExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMySqlExample1Application.class, args);
+	}
+
+}
